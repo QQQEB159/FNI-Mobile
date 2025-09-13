6a91307da0f72bfa84c633033b81005aa6349e15
tchcfhvgd <2220687506@qq.com>
2025-09-13 13:04:12 +0800

    lol

--- source/objects/CharacterPortrait.hx
+++ source/objects/CharacterPortrait.hx
@@ -4,44 +4,39 @@ class CharacterPortrait extends FlxSprite
 {
 	public var pJson:Dynamic;
 	
-	public function new(image:String) {
-		super();
+	public function new(portraitName:String) {
+		super(0, 0);
 
-	    loadWithData(image);
+	    changePortrait(portraitName);
 	}
-
-	private function loadWithData(image:String) {
-        try {
-            loadGraphic(Paths.image('menus/story-freeplay/portraits/' + image));
-            antialiasing = ClientPrefs.data.antialiasing;
+	
+	public function changePortrait(newPortrait:String):Void
+    {
+           loadGraphic(Paths.image('menus/story-freeplay/portraits/$newPortrait'));
+           antialiasing = ClientPrefs.data.antialiasing;
             
-            var jsonPath:String = Paths.json('menus/story-freeplay/portraits/' + image);
+            pJson = {offsets: [0, 0], bgSprite: "placeholder", scale: [0, 0], flipTween: false};
+            
+            var jsonPath:String = Paths.json('menus/story-freeplay/portraits/$newPortrait');
             if(Paths.fileExists(jsonPath, TEXT)) {
                 var rawJson:String = Paths.getTextFromFile(jsonPath);
                 pJson = haxe.Json.parse(rawJson);
-                applyJsonConfig();
            }
-        }
+            
+            if (pJson.flipTween == null) {
+                pJson.flipTween = false;
+            }
+            
+            if (pJson.bgSprite == null) {
+                pJson.bgSprite = "default";
+            }
+            
+            if (pJson.scale != null) {
+               scale.set(pJson.scale[0], pJson.scale[1]);
+            }
+            
+            if (pJson.offsets != null) {
+               offset.set(pJson.offsets[0], pJson.offsets[1]);
+            }
     }
-	
-	private function applyJsonConfig() {
-        if (pJson == null) return;
-        
-        if (pJson.offsets != null) {
-            offset.set(pJson.offsets[0], pJson.offsets[1]);
-        }
-        
-        if (pJson.scale != null) {
-            scale.set(pJson.scale[0], pJson.scale[1]);
-            updateHitbox();
-        }
-        
-        if (pJson.flipTween == null) {
-            pJson.flipTween = false;
-        }
-    }
-	
-	public function changePortrait(image:String) {
-		loadWithData(image);
-	}
 }
